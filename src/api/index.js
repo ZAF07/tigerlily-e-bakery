d75b17f10223690ca4afa22cc882f75eae85eb8c
ZAF07 <zaffere07@gmail.com>
2022-05-16 01:10:23 +0800

    tested new payment type

--- src/api/index.js
+++ src/api/index.js
@@ -25,7 +25,7 @@ export const checkout = (a) => {
     customer_id: "9388",
     discount_code: "00112233"
   }]
-  const paymentType = 'stripe_checkout_session'
+  const paymentType = 'test_strategy'
   axios.post(checkoutURL, {checkout_items: mockData, payment_type: paymentType}, {
     headers: {
       'content-type': 'text/json'
